d23dc41d83dbf9ed16e24f8c04231d6492004b01
belmontrobotics6731 <32988702+belmontrobotics6731@users.noreply.github.com>
2024-01-18 19:25:43 -0500

    Aquisition setup

---
+++ src/main/java/frc/robot/commands/ManualAcquisition.java
@@ -0,0 +1,41 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.control.IControlInput;
+import frc.robot.subsystems.Acquisition;
+
+public class ManualAcquisition extends CommandBase{
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+    private Acquisition _acquisition;
+    private IControlInput _controls;
+
+
+     public ManualAcquisition(Acquisition acquisition, IControlInput controls) {
+        _acquisition = acquisition;
+        _controls = controls;
+        addRequirements(acquisition);
+    }
+
+    // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // TODO call function to run motor
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
+ 
