1d39ed356bb0cf7cbae77a8efe4e453bccb4dbd2
GoatHero <72709281+GoatHero@users.noreply.github.com>
2024-02-01 19:29:48 -0500

    more updates

--- src/main/java/frc/robot/commands/ManualClimbers.java
+++ src/main/java/frc/robot/commands/ManualClimbers.java
@@ -29,6 +29,7 @@ public class ManualClimbers extends Command{
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    _climbers.set(false);
   }
 
   // Returns true when the command should end.
