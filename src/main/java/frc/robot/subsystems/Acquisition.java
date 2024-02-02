4b57f87938f59d964d2a5c3c75128fd417276980
GoatHero <72709281+GoatHero@users.noreply.github.com>
2024-02-01 20:38:14 -0500

    Update Acquisition.java

--- src/main/java/frc/robot/subsystems/Acquisition.java
+++ src/main/java/frc/robot/subsystems/Acquisition.java
@@ -1,4 +1,4 @@
-public class Acquisition {
+
     // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -39,6 +39,3 @@ public class Acquisition extends SubsystemBase {
         }
     }
 
-
-    
-}
