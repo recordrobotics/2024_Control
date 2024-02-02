747d6b9e99b9ee2f0c66225ff5292fd8fc0ea9d3
GoatHero <72709281+GoatHero@users.noreply.github.com>
2024-02-01 20:38:46 -0500

    Update Acquisition.java

--- src/main/java/frc/robot/subsystems/Acquisition.java
+++ src/main/java/frc/robot/subsystems/Acquisition.java
@@ -1,5 +1,5 @@
 
-    // Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -17,11 +17,11 @@ public class Acquisition extends SubsystemBase {
     private static final double LOWER_SPEED = 0.3;
     private static final double UPPER_SPEED = 0.1;
 
-    public Shooter() {
-        toggle(ShooterStates.STOP);
+    public Acquisition() {
+        toggle(Shooter.ShooterStates.STOP);
     }
 
-    public void toggle(ShooterStates state) {
+    public void toggle(Shooter.ShooterStates state) {
         switch (state) {
             case FLYWHEEL:
                 lower.set(0);
@@ -35,7 +35,6 @@ public class Acquisition extends SubsystemBase {
                 lower.set(0);
                 upper.set(0);
                 break;
-        }   
         }
     }
-
+}
