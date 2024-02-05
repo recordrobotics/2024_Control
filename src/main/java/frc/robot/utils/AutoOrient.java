ec771a9646461105ce3de771461c48683612ef87
GoatHero <72709281+GoatHero@users.noreply.github.com>
2024-02-05 18:37:06 -0500

    Add comments

--- src/main/java/frc/robot/utils/AutoOrient.java
+++ src/main/java/frc/robot/utils/AutoOrient.java
@@ -4,6 +4,15 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
 public class AutoOrient {
+
+    /**
+     * Calculates the correct angle to move the robot to in order to face the target
+     * position
+     * 
+     * @param robotPosition
+     * @param targetPosition
+     * @return
+     */
     public static Rotation2d rotationFacingTarget(Translation2d robotPosition, Translation2d targetPosition) {
         return new Rotation2d(
                 Math.atan2(robotPosition.getY() - targetPosition.getY(), robotPosition.getX() - targetPosition.getX()));
