4ddcd341e994289a5e36d7c9222c972cb0aeab40
scaryjoshie <scaryjoshie@gmail.com>
2024-02-07 12:51:42 -0500

    Documentation Changes

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -3,9 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -14,8 +12,10 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 public class SwerveModule {
 
@@ -54,53 +54,56 @@ public class SwerveModule {
       int absoluteTurningMotorEncoderChannel,
       double turningEncoderOffset) {
 
+    // Creates TalonFX objects
     m_driveMotor = new TalonFX(driveMotorChannel);
     m_turningMotor = new TalonFX(turningMotorChannel);
+
+    // Creates Motor Encoder object and gets offset
     absoluteTurningMotorEncoder = new DutyCycleEncoder(absoluteTurningMotorEncoderChannel);
     this.turningEncoderOffset = turningEncoderOffset;
 
+    // 3 Seconds delay per swerve module
     Timer.delay(3);
 
+    // Sets motor speeds to 0
     m_driveMotor.set(0);
     m_turningMotor.set(0);
 
-    // Limit the PID Controller's input range between -0.5 and 0.5 and set the input
-    // to be continuous.
+    // Limit the PID Controller's input range between -0.5 and 0.5 and set the input to be continuous.
     m_turningPIDController.enableContinuousInput(-0.5, 0.5);
 
     // Corrects for offset in absolute motor position
-    m_turningMotor.setPosition(getAbsWheelOffset());
-    SmartDashboard.putNumber("E" + Double.toString(absoluteTurningMotorEncoderChannel),
-        absoluteTurningMotorEncoder.getAbsolutePosition());
+    m_turningMotor.setPosition(getAbsWheelTurnOffset());
+    
+    //SmartDashboard.putNumber("E" + Double.toString(absoluteTurningMotorEncoderChannel), absoluteTurningMotorEncoder.getAbsolutePosition());
   }
 
+
   /**
+   * custom function
    * @return The current offset absolute position of the wheel's turn
    */
-  private double getAbsWheelOffset() {
-
+  private double getAbsWheelTurnOffset() {
     double absEncoderPosition = (absoluteTurningMotorEncoder.getAbsolutePosition() - turningEncoderOffset + 1) % 1;
-    // Inverts absEncoderPosition as the relative encoders and the absoulte encoders
-    // increase in different directions
-    // so the offset needs to be inverted
     double absWheelPositionOffset = absEncoderPosition * Constants.Swerve.DIRECTION_GEAR_RATIO;
     return absWheelPositionOffset;
   }
 
   /**
+   * TODO: figure out how this calculation works and make it more clear instead of having it all happen on one line
+   * custom function
    * @return The current velocity of the drive motor (meters per second)
    */
   private double getDriveWheelVelocity() {
     double driveMotorRotationsPerSecond = m_driveMotor.getVelocity().getValue();
     double driveWheelMetersPerSecond = driveMotorRotationsPerSecond * 10 / Constants.Swerve.RELATIVE_ENCODER_RATIO
-        * (Constants.Swerve.SWERVE_WHEEL_DIAMETER * Math.PI); // TODO: was originally 0.05 * 2 * pi. Also, why do we
-                                                              // multiple by 10 here?
+        * (Constants.Swerve.SWERVE_WHEEL_DIAMETER * Math.PI); 
     return driveWheelMetersPerSecond;
   }
 
   /**
-   * @return The raw rotations of the turning motor (rotation 2d object). NOT THE
-   *         WHEEL. THE MOTOR.
+   * custom function
+   * @return The raw rotations of the turning motor (rotation 2d object). NOT THE WHEEL. THE MOTOR.
    */
   private Rotation2d getTurnWheelRotation2d() {
     double numMotorRotations = m_turningMotor.getPosition().getValue();
@@ -109,6 +112,7 @@ public class SwerveModule {
   }
 
   /**
+   * custom function
    * @return The number of rotations of the turning wheel (rotations)
    */
   private double getTurnWheelRotations() {
@@ -118,16 +122,18 @@ public class SwerveModule {
   }
 
   /**
+   * custom function
    * @return The distance driven by the drive wheel (meters)
    */
   private double getDriveWheelDistance() {
-    double numRotationsMotor = m_driveMotor.getPosition().getValue();
-    double numRotationsWheel = numRotationsMotor / Constants.Swerve.SPEED_GEAR_RATIO;
-    double speedWheelDistanceMeters = numRotationsWheel * Math.PI * Constants.Swerve.SWERVE_WHEEL_DIAMETER;
+    double numRotationsDriveMotor = m_driveMotor.getPosition().getValue();
+    double numRotationsDriveWheel = numRotationsDriveMotor / Constants.Swerve.SPEED_GEAR_RATIO;
+    double speedWheelDistanceMeters = numRotationsDriveWheel * Math.PI * Constants.Swerve.SWERVE_WHEEL_DIAMETER;
     return speedWheelDistanceMeters;
   }
 
   /**
+   * custom function
    * @return The current state of the module.
    */
   public SwerveModuleState getModuleState() {
@@ -136,7 +142,8 @@ public class SwerveModule {
   }
 
   /**
-   * @return The current position of the module.
+   * custom function
+   * @return The current position of the module as a SwerveModulePosition object.
    */
   public SwerveModulePosition getModulePosition() {
     return new SwerveModulePosition(
