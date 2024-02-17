8b2581b327c0a416a5ef754af2e997a08186eb2c
GoatHero <72709281+GoatHero@users.noreply.github.com>
2024-02-17 14:32:13 -0500

    Add autodrive completed print

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -121,7 +121,9 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return new AutoDrive(_drivetrain);
+    return new AutoDrive(_drivetrain).andThen(() -> {
+      System.out.println("AutoDrive completed");
+    }, _drivetrain);
   }
 
   public void testSwerve() {
