01e6f3c5aeb296b1b9316edefa929cc674287425
scaryjoshie <scaryjoshie@gmail.com>
2024-02-25 17:40:30 -0500

    ABDASDASD

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -157,32 +157,32 @@ public final class Constants {
                 1,
                 0.597,
                 frontLeftLocation,
-                MotorType.Falcon,
-                MotorType.Falcon);
+                MotorType.Kraken,
+                MotorType.Kraken);
         public static final ModuleConstants frontRightConstants = new ModuleConstants(
                 4,
                 3,
                 2,
                 0.886,
                 frontRightLocation,
-                MotorType.Falcon,
-                MotorType.Falcon);
+                MotorType.Kraken,
+                MotorType.Kraken);
         public static final ModuleConstants backLeftConstants = new ModuleConstants(
                 8,
                 7,
                 4,
                 0.857,
                 backLeftLocation,
-                MotorType.Falcon,
-                MotorType.Falcon);
+                MotorType.Kraken,
+                MotorType.Kraken);
         public static final ModuleConstants backRightConstants = new ModuleConstants(
                 6,
                 5,
                 3,
                 0.556,
                 backRightLocation,
-                MotorType.Falcon,
-                MotorType.Falcon);
+                MotorType.Kraken,
+                MotorType.Kraken);
     }
 
     public final class Frame {
