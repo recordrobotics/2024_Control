15f55581f72d37ca2010f4eea5ff35cde0d0f76f
scaryjoshie <scaryjoshie@gmail.com>
2024-02-29 20:48:57 -0500

    Update RobotKill.java

--- src/main/java/frc/robot/commands/RobotKill.java
+++ src/main/java/frc/robot/commands/RobotKill.java
@@ -4,7 +4,17 @@
 
 package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Acquisition;
+import frc.robot.subsystems.Channel;
+import frc.robot.subsystems.Climbers;
+import frc.robot.subsystems.Crashbar;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Acquisition.AcquisitionStates;
+import frc.robot.subsystems.Channel.ChannelStates;
+import frc.robot.subsystems.Climbers.ClimberStates;
+import frc.robot.subsystems.Crashbar.CrashbarStates;
+import frc.robot.subsystems.Shooter.ShooterStates;
 
 /**
  * A command that does nothing but takes a specified amount of time to finish.
@@ -13,10 +23,26 @@ import frc.robot.subsystems.Drivetrain;
  */
 public class RobotKill extends Command {
   private Drivetrain _drivetrain;
+  private Acquisition _acquisition;
+  private Channel _channel;
+  private Climbers _climbers;
+  private Shooter _shooter;
+  private Crashbar _crashbar;
+
   
 
-  public RobotKill(Drivetrain drivetrain) {
+  public RobotKill(Drivetrain drivetrain, 
+                   Acquisition acquisition, 
+                   Channel channel, 
+                   Climbers climbers, 
+                   Shooter shooter, 
+                   Crashbar crashbar) {
     _drivetrain = drivetrain;
+    _acquisition = acquisition;
+    _channel = channel;
+    _climbers = climbers;
+    _shooter = shooter;
+    _crashbar = crashbar;
   }
 
   @Override
@@ -26,6 +52,11 @@ public class RobotKill extends Command {
   @Override
   public void execute () {
     _drivetrain.stop();
+    _acquisition.toggle(AcquisitionStates.OFF);
+    _channel.toggle(ChannelStates.OFF);
+    _climbers.toggle(ClimberStates.OFF);
+    _shooter.toggle(ShooterStates.OFF);
+    _crashbar.toggle(CrashbarStates.OFF);
   }
 
   @Override
