eb32f5791a24ac2c0caf26834df6ad79bc151e75
GoatHero <72709281+GoatHero@users.noreply.github.com>
2024-03-09 17:31:56 -0500

    Update Vision.java

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -20,39 +20,7 @@ public class Vision extends SubsystemBase {
         camera = new PhotonCamera(Constants.Vision.cameraID);
     }
 
-    // public void periodic()  {
-    //     // Gets a frame from the camera
-	// 	var result = camera.getLatestResult();
-    // }
-
-	public static Pose2d estimateGlobalPose() {
-
-		// Gets a frame from the camera
-		var result = camera.getLatestResult();
-
-		// Gets target object from apriltag perspective photonvision
-		PhotonTrackedTarget target = result.getBestTarget();
-
-		// Gets the Transform3d object for april to robot
-		Transform3d robot_to_april = target.getBestCameraToTarget()/*.plus(robotToCam.inverse())*/; // you could put the offset here if you were testing for reals
-		Transform3d april_to_robot = robot_to_april.inverse();
-
-		// Gets the fiducial ID and uses it to get the correct transform 2d object
-		int targetID = target.getFiducialId();
-		Transform3d global_to_april = Constants.Vision.tagTransforms[targetID - 1];
-
-		// Adds the two transform objects together to get robot to global
-		Transform3d global_to_robot = global_to_april.plus(april_to_robot);
-
-        // Calculates the robot's global pose
-        Pose2d robotGlobalPose = new Pose2d(
-            new Translation2d(global_to_robot.getX(), global_to_robot.getY()),
-            new Rotation2d(global_to_robot.getRotation().getZ())
-        );
-
-        // Returns
-        return robotGlobalPose;
-    }
+    
 
     public Rotation2d ringDirection(){
 		// Gets target object orientation from orange photonvision
@@ -64,13 +32,13 @@ public class Vision extends SubsystemBase {
             }
     }
 
-	public static boolean checkForTarget(){
+	public boolean checkForTarget(){
 		var result = camera.getLatestResult();//get a frame from the camera
 		boolean hasTargets = result.hasTargets();//check for targets. This MUST be checked for, otherwise an error will occur if there isn't a target.
 		return hasTargets;
 	}
 
-    public static double getTargetID() {
+    public double getTargetID() {
         var result = camera.getLatestResult();
         PhotonTrackedTarget target = result.getBestTarget();
         int targetID = target.getFiducialId();
