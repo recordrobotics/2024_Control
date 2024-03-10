21297273578559ab3c5faa0d3ff00c6da4d83457
GoatHero <148826326+Yunqiy@users.noreply.github.com>
2024-03-10 09:26:30 -0400

    remove vision

--- src/main/java/frc/robot/commands/hybrid/GetNote.java
+++ src/main/java/frc/robot/commands/hybrid/GetNote.java
@@ -1,50 +1,50 @@
-package frc.robot.commands.hybrid;
+// package frc.robot.commands.hybrid;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Acquisition;
-import frc.robot.subsystems.Channel;
-import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Photosensor;
-import frc.robot.subsystems.Vision;
-import frc.robot.utils.DriveCommandData;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import frc.robot.subsystems.Acquisition;
+// import frc.robot.subsystems.Channel;
+// import frc.robot.subsystems.Drivetrain;
+// import frc.robot.subsystems.Photosensor;
+// import frc.robot.subsystems.Vision;
+// import frc.robot.utils.DriveCommandData;
 
 
-public class GetNote extends Command{
-    Drivetrain driveTrain;
-    Vision vision;
-    double speed;
-    Photosensor _photosensor;
-    Acquisition _acquisition;
-    Channel _channel;
+// public class GetNote extends Command{
+//     Drivetrain driveTrain;
+//     Vision vision;
+//     double speed;
+//     Photosensor _photosensor;
+//     Acquisition _acquisition;
+//     Channel _channel;
 
-    public GetNote(Drivetrain drivetrain, Vision vision, double speed, Photosensor photosensor, Acquisition acquisition, Channel channel){
-        addRequirements(drivetrain);
-        addRequirements(acquisition);
-        addRequirements(channel);
-        addRequirements(photosensor);
+//     public GetNote(Drivetrain drivetrain, Vision vision, double speed, Photosensor photosensor, Acquisition acquisition, Channel channel){
+//         addRequirements(drivetrain);
+//         addRequirements(acquisition);
+//         addRequirements(channel);
+//         addRequirements(photosensor);
 
-        this.driveTrain = drivetrain;
-        this.vision = vision;
-        this.speed = speed;
+//         this.driveTrain = drivetrain;
+//         this.vision = vision;
+//         this.speed = speed;
 
-        _acquisition = acquisition;
-        _channel = channel;
-        _photosensor = photosensor;
-    }
+//         _acquisition = acquisition;
+//         _channel = channel;
+//         _photosensor = photosensor;
+//     }
 
-    @Override
-    public void execute(){
-        driveTrain.drive(new DriveCommandData(speed, 0, 0, false));
+//     @Override
+//     public void execute(){
+//         driveTrain.drive(new DriveCommandData(speed, 0, 0, false));
         
-    }
+//     }
 
-    @Override
-    public boolean isFinished(){
-        return _photosensor.getDebouncedValue();
-    }
+//     @Override
+//     public boolean isFinished(){
+//         return _photosensor.getDebouncedValue();
+//     }
 
-    @Override
-    public void end(boolean interrupted) {
-        driveTrain.stop();
-    }
-}
+//     @Override
+//     public void end(boolean interrupted) {
+//         driveTrain.stop();
+//     }
+// }
--- src/main/java/frc/robot/commands/hybrid/NoteOrient.java
+++ src/main/java/frc/robot/commands/hybrid/NoteOrient.java
@@ -1,49 +1,49 @@
-package frc.robot.commands.hybrid;
+// package frc.robot.commands.hybrid;
 
-import java.util.function.BooleanSupplier;
+// import java.util.function.BooleanSupplier;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Photosensor;
-import frc.robot.subsystems.Vision;
-import frc.robot.utils.DriveCommandData;
+// import edu.wpi.first.math.controller.PIDController;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import frc.robot.subsystems.Drivetrain;
+// import frc.robot.subsystems.Photosensor;
+// import frc.robot.subsystems.Vision;
+// import frc.robot.utils.DriveCommandData;
 
-public class NoteOrient extends Command{
+// public class NoteOrient extends Command{
 
-    Drivetrain driveTrain;
-    Vision vision;
-    PIDController anglePID;
-    Photosensor photosensor;
-    BooleanSupplier button;
+//     Drivetrain driveTrain;
+//     Vision vision;
+//     PIDController anglePID;
+//     Photosensor photosensor;
+//     BooleanSupplier button;
 
-    public NoteOrient(Drivetrain drivetrain, Vision vision, Photosensor photosensor, BooleanSupplier button){
-        addRequirements(drivetrain);
-        setSubsystem(drivetrain.getName());
-        this.driveTrain = drivetrain;
-        this.vision = vision;
-        this.photosensor = photosensor;
+//     public NoteOrient(Drivetrain drivetrain, Vision vision, Photosensor photosensor, BooleanSupplier button){
+//         addRequirements(drivetrain);
+//         setSubsystem(drivetrain.getName());
+//         this.driveTrain = drivetrain;
+//         this.vision = vision;
+//         this.photosensor = photosensor;
 
-        anglePID = new PIDController(2, 0, 0);
-        anglePID.enableContinuousInput(-Math.PI, Math.PI);
-    }
+//         anglePID = new PIDController(2, 0, 0);
+//         anglePID.enableContinuousInput(-Math.PI, Math.PI);
+//     }
 
-    @Override
-    public void initialize() {
-        driveTrain.stop();
-    }
+//     @Override
+//     public void initialize() {
+//         driveTrain.stop();
+//     }
 
-    @Override
-    public void execute() {
-        driveTrain.drive(new DriveCommandData(0, 0, anglePID.calculate(vision.ringDirection().getRadians()), false));
-    }
+//     @Override
+//     public void execute() {
+//         driveTrain.drive(new DriveCommandData(0, 0, anglePID.calculate(vision.ringDirection().getRadians()), false));
+//     }
 
-    @Override
-    public boolean isFinished(){
-        return (vision.ringDirection().getRadians() < 0.1 && vision.ringDirection().getRadians() > -0.1) || !button.getAsBoolean();
-    }
+//     @Override
+//     public boolean isFinished(){
+//         return (vision.ringDirection().getRadians() < 0.1 && vision.ringDirection().getRadians() > -0.1) || !button.getAsBoolean();
+//     }
 
-    @Override
-    public void end(boolean interrupted) {
-    }
-}
+//     @Override
+//     public void end(boolean interrupted) {
+//     }
+// }
