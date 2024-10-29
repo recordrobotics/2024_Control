189e3c32057f41d07ad4f3946130615c5a548868
Isaac <barkerisaacd@gmail.com>
2024-10-28 20:54:05 -0400

    shooter -> this.shooter

--- src/test/java/ShooterTest.java
+++ src/test/java/ShooterTest.java
@@ -15,12 +15,12 @@ public class ShooterTest {
     @BeforeEach
     public void setup() {
         assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
-        shooter = new Shooter();
+        this.shooter = new Shooter();
     }
 
     @AfterEach
     public void shutdown() throws Exception {
-        shooter.close();
+        this.shooter.close();
     }
 
     private void assertSpeed(double speed, String msg) {
@@ -28,8 +28,8 @@ public class ShooterTest {
     }
 
     private void assertSpeed(double speedL, double speedR, String msg) {
-        assertEquals(-speedL, shooter.flywheelL.get(), TOLERANCE, msg);
-        assertEquals(speedR, shooter.flywheelR.get(), TOLERANCE, msg);
+        assertEquals(-speedL, this.shooter.flywheelL.get(), TOLERANCE, msg);
+        assertEquals(speedR, this.shooter.flywheelR.get(), TOLERANCE, msg);
     }
 
     @Test
@@ -39,51 +39,51 @@ public class ShooterTest {
 
     @Test
     public void testToggleSpeakerState() {
-        shooter.toggle(Shooter.ShooterStates.SPEAKER);
+        this.shooter.toggle(Shooter.ShooterStates.SPEAKER);
         assertSpeed(Constants.Shooter.SPEAKER_SPEED, "Motor should be set to default SPEAKER speed.");
     }
 
     @Test
     public void testToggleAmpState() {
-        shooter.toggle(Shooter.ShooterStates.AMP);
+        this.shooter.toggle(Shooter.ShooterStates.AMP);
         assertSpeed(Constants.Shooter.AMP_SPEED, "Motor should be set to default AMP speed.");
     }
 
     @Test
     public void testToggleReverseState() {
-        shooter.toggle(Shooter.ShooterStates.REVERSE);
+        this.shooter.toggle(Shooter.ShooterStates.REVERSE);
         assertSpeed(Constants.Shooter.REVERSE_SPEED, "Motor should be set to default REVERSE speed.");
     }
 
     @Test
     public void testToggleCustomState() {
-        shooter.toggle(1.5);
+        this.shooter.toggle(1.5);
         assertSpeed(1, "Motor should not go above 1 speed");
     }
 
     @Test
     public void testToggleNegativeCustomState() {
-        shooter.toggle(-1.5);
+        this.shooter.toggle(-1.5);
         assertSpeed(-1, "Motor should not go below -1 speed");
     }
 
     @Test
     public void testLeftPositiveRightNegative() {
-        shooter.toggle(1, -1);
+        this.shooter.toggle(1, -1);
         assertSpeed(1, -1, "Left forward right backward");
     }
 
     @Test
     public void testToggleOffState() {
-        shooter.toggle(Shooter.ShooterStates.SPEAKER); // Set to IN first
-        shooter.toggle(Shooter.ShooterStates.OFF); // Now set to OFF
+        this.shooter.toggle(Shooter.ShooterStates.SPEAKER); // Set to IN first
+        this.shooter.toggle(Shooter.ShooterStates.OFF); // Now set to OFF
         assertSpeed(0, "Motor should be off when state is OFF.");
     }
 
     @Test
     public void testKill() {
-        shooter.toggle(Shooter.ShooterStates.SPEAKER); // Activate the motor
-        shooter.kill(); // Kill should turn off the motor
+        this.shooter.toggle(Shooter.ShooterStates.SPEAKER); // Activate the motor
+        this.shooter.kill(); // Kill should turn off the motor
         assertSpeed(0, "Motor should be off after kill.");
     }
 }
